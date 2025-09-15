b93cc2f2fb6477810467183ec7d6b03caf22c822
riya <riya.ueng@gmail.com>
2025-09-15 16:10:28 +0200

    pokemondetails_base_created

--- src/components/pokemonList/PokemonList.tsx
+++ src/components/pokemonList/PokemonList.tsx
@@ -3,8 +3,9 @@ import type { IPokemon } from "../../interfaces/IPokemon"
 import "./PokemonList.css"
 import { dummyPokemon } from "../../data/data"
 
-export default function PokeminList() {
+export default function PokemonList() {
   const [listedPokemon, setListedPokemon] = useState<IPokemon[]>(dummyPokemon)
+
   return (
     <>
       <section className="section_pokemonlist">
