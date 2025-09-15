5620b88fd5bfa1ee0ec5199a81d85be32f5965c8
riya <riya.ueng@gmail.com>
2025-09-15 14:58:30 +0200

    component_pokemonlist_created

--- src/components/pokemonList/PokeminList.tsx
+++ src/components/pokemonList/PokeminList.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react"
+import type { IPokemon } from "../../interfaces/IPokemon"
 import "./PokemonList.css"
+import { dummyPokemon } from "../../data/data"
 
 export default function PokeminList() {
-  return <div>PokeminList</div>
+  const [listedPokemon, setListedPokemon] = useState<IPokemon[]>(dummyPokemon)
+  return (
+    <>
+      <section className="section_pokemonlist">
+        {listedPokemon.map((pokemon, index) => {
+          return (
+            <div key={index}>
+              <PokemonCard pokemon={pokemon} />
+            </div>
+          )
+        })}
+      </section>
+    </>
+  )
 }
