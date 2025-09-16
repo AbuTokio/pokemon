8ea1905dc4d3db1a111ee705f24f213a81d3bef8
AbuTokio <bunyaminxkara@gmail.com>
2025-09-16 11:25:10 +0200

    PokemonProvider and interfaces

--- src/context/PokemonProvider.tsx
+++ src/context/PokemonProvider.tsx
@@ -7,15 +7,15 @@ export const pokemonContext = createContext<PokemonProviderProps | null>(null)
 export interface PokemonProviderProps {
   searchTerm: string
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>
-  filterType: string
-  setFilterType: React.Dispatch<React.SetStateAction<string>>
+  filterType: number
+  setFilterType: React.Dispatch<React.SetStateAction<number>>
   selectedPokemon: IPokemon | null
   setSelectedPokemon: React.Dispatch<React.SetStateAction<IPokemon | null>>
 }
 
 export default function PokemonProvider({ children }: { children: React.ReactNode }) {
   const [searchTerm, setSearchTerm] = useState("")
-  const [filterType, setFilterType] = useState("")
+  const [filterType, setFilterType] = useState(0)
   const [selectedPokemon, setSelectedPokemon] = useState<IPokemon | null>(null)
 
   return (
