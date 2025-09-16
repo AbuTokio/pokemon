b0004b93f96dc82afc5b7b8afdab7f98d8b7ac6b
riya <riya.ueng@gmail.com>
2025-09-16 11:08:47 +0200

    grid_home_header_footer_base_created

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router"
 import "../layout/Layout.css"
+import Header from "../components/header/Header"
+import Footer from "../components/footer/Footer"
 
 export default function Layout() {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
